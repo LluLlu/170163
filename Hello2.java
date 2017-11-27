745b584069f052d4360a436dfaf53be0a3b3c106
LluLlu <33210528+LluLlu@users.noreply.github.com>
2017-11-27 10:29:30 +0900

    Hello2 java

--- Hello2.java
+++ Hello2.java
@@ -1,24 +1,11 @@
 import javax.swing.*;
-import java.awt.Container;
-import java.awt.BorderLayout;
+import java.awt.*;
 
-public class Hello2 extends JFrame{
-	Hello2(String title){
-		setTitle(title);
-		setSize(400,400);
-		setLocation(400,400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JButton btn = new JButton("Hello");
-
-        JPanel panel = new JPanel();
-        panel.add(btn);
-
-        Container cp = getContentPane();
-        cp.add(panel,BorderLayout.CENTER);
-	}
+public class Hello2{
 	public static void main(String[] args){
-	  Hello hello = new Hello("ALOHA");
-		hello.setVisible(true);
+	  JFrame j = new JFrame("Hello2");
+	  j.setBounds(500,500,500,500);
+	  j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
+	  j.setVisible(true);
 	}
 }
